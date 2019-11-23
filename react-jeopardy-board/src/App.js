dc93bf57c825266b78429582b37ae3b699b5cb0d
Teddy Karambelas <tvkarambelas@gmail.com>
2019-11-23 10:46:19 -0500

    adjusts categories, adds logic and styling for ActiveClue

--- react-jeopardy-board/src/App.js
+++ react-jeopardy-board/src/App.js
@@ -2,25 +2,26 @@ import React, {useState, useEffect} from 'react';
 
 function JeopardyBoard() {
   const [cluesData, setCluesData] = useState('')
+  const [activeClue, setActiveClue] = useState('')
 
   useEffect(() => {
     async function getCluesData() {
-      const getCat1 = await window.fetch('http://jservice.io/api/category/?id=196')
+      const getCat1 = await window.fetch('http://jservice.io/api/category/?id=50')
       const cat1Data = await getCat1.json()
 
       const getCat2 = await window.fetch('http://jservice.io/api/category/?id=100')
       const cat2Data = await getCat2.json()
 
-      const getCat3 = await window.fetch('http://jservice.io/api/category/?id=24')
+      const getCat3 = await window.fetch('http://jservice.io/api/category/?id=125')
       const cat3Data = await getCat3.json()
 
       const getCat4 = await window.fetch('http://jservice.io/api/category/?id=44')
       const cat4Data = await getCat4.json()
 
-      const getCat5 = await window.fetch('http://jservice.io/api/category/?id=16694')
+      const getCat5 = await window.fetch('http://jservice.io/api/category/?id=175')
       const cat5Data = await getCat5.json()
 
-      const getCat6 = await window.fetch('http://jservice.io/api/category/?id=9556')
+      const getCat6 = await window.fetch('http://jservice.io/api/category/?id=200')
       const cat6Data = await getCat6.json()
 
       setCluesData(
@@ -36,47 +37,83 @@ function JeopardyBoard() {
     }
     getCluesData()
   }, [])  
+
+  function handleActiveClue(e, clueData) {
+    e.preventDefault();
+    setActiveClue(clueData);
+  }
   
   return (
     <>
       <h1>Jeopardy!</h1>
 
-      <div id="jeopardy-board">
-        {cluesData ? 
-          cluesData.map(catData => (
-            <Category key={catData.id} catData={catData} />
-          )) 
-          : ''
-        }
-      </div>
+      <div id="board-container">
+        <div id="board">
+          {cluesData ? 
+            cluesData.map(catData => (
+              <div key={catData.id} className="category">
+                <div className="title">{catData.title}</div>
+
+                {catData.clues.slice(0,5).map(clueData => (
+                  <Clue key={clueData.id} clueData={clueData} handleActiveClue={handleActiveClue} />
+                ))}
+              </div>
+            )) 
+            : ''
+          }
+        </div>
+
+        <ActiveClue clueData={activeClue} handleActiveClue={handleActiveClue} />
+      </div>  
     </>
   )
 }
 
-function Category({ catData }) {
+function Clue({ clueData, handleActiveClue }) {
   return (
     <>
-      <div className="category">
-        <div className="title">{catData.title}</div>
-
-        {catData.clues.slice(0,5).map(clueData => (
-          <Clue key={clueData.id} clueData={clueData} />
-        ))}
-      </div>
-    </>
-  )
-}
-
-function Clue({ clueData }) {
-  return (
-    <>
-      <button className="clue">
+      <button className="clue" onClick={e => handleActiveClue(e,clueData)}>
         <div className="value">${clueData.value}</div>
 
         <div className="details">
           <div className="ques">{clueData.question}</div>
         </div>
       </button>
+    </>
+  )
+}
+
+function ActiveClue({clueData, handleActiveClue}) {
+  const [clueState, setClueState] = useState('question')
+
+  function closeClue(e) {
+    e.preventDefault();
+    setClueState('question')
+    handleActiveClue(e,null)
+  }
+
+  return (
+    <>
+      {clueData ?
+        <div id="active-clue-container">
+          <div id="active-clue">
+            {clueState === 'question' ?
+              <>
+                <div className="clue">{clueData.question}</div>
+                <button onClick={e => setClueState('answer')}>Show Answer</button>
+              </>
+              :
+              <>
+                <div className="answer">{clueData.answer}</div>
+                <button onClick={e => setClueState('question')}>Show Clue</button>
+              </>
+            }
+                        
+            <button onClick={e => closeClue(e)}>Close</button>
+          </div>
+        </div>
+        : ''
+      }
     </>
   )
 }
